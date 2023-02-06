c8733f507378335492c59ac5dad3650bf7d6a7ba
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-06 06:03:29 -0800

    setting up models, finished Blog model

---
+++ models/Blogs.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Blogs extends Model {}
+
+Blogs.init(
+  {
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    blog_user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Users",
+        key: "user_id",
+      },
+    },
+    blog_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    blog_data: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Blogs",
+  }
+);
+
+module.exports = Blogs;
