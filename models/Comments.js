d56a2d9088c830f8e019ca366d391b0249fd1f25
Mohamed_Farah <mohamedfarah0508@gmail.com>
2023-02-08 04:32:33 -0800

    starting to add the html, css and main js

--- models/Comments.js
+++ models/Comments.js
@@ -1,2 +1,38 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+Comments.init(
+  {
+    comment_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Blogs",
+        key: "blog_id",
+      },
+    },
+    comment_user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Users",
+        key: "user_id",
+      },
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Comments",
+  }
+);
