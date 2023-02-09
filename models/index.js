799bcad60d70f08c1e9fcfa81be83087086490ab
Mohamed_Farah <mohamedfarah0508@gmail.com>
2023-02-09 02:38:42 -0800

    convertinh html to handlebars

---
+++ models/index.js
@@ -0,0 +1,26 @@
+// Requiring models.
+const User = require("./UserData");
+const Blogs = require("./Blogs");
+const Comments = require("./Comments");
+
+// Configuring model association.
+User.hasMany(Blogs, {
+  foreignKey: "blogs_id",
+});
+Blogs.hasOne(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comments, {
+  foreignKey: "comment_user_id",
+});
+
+Comments.belongsTo(Blogs, {
+  foreignKey: "blogs_id",
+});
+Blogs.hasMany(Comments, {
+  foreignKey: "blog_id",
+});
+
+// Exporting models.
+module.exports = { User, Blogs, Comment };
--- server.js
+++ server.js
@@ -1,46 +1,30 @@
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
+const app = express();
+const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 3001;
 const path = require("path");
 
-const helpers = require("./utils/helpers");
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+  })
+);
 
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers });
-
-const session = require("express-session");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-const sess = {
-  secret: "KULILINSAAN",
-  cookie: {
-    // Session will automatically expire in 10 minutes
-    expires: 10 * 60 * 1000,
-  },
-  resave: true,
-  rolling: true,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-app.use(session(sess));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
-app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(routes);
+app.use(express.static("views"));
 
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+app.get("/", (req, res) => {
+  res.render("layouts/main", { title: "Tech Blog - Home" });
+});
+app.get("/signUp", (req, res) => {
+  res.render("signUpPage", { title: "Tech Blog - Sign Up" });
+});
+app.get("/signIn", (req, res) => {
+  res.render("signInPage", { title: "Tech Blog - Sign In" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
