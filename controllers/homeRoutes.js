f479e07438fe1d62b1384f9dccd2ae079a08fc0a
Mohamed Farah <mohamedfarah0508@gmail.com>
2023-02-28 16:12:24 -0800

    app isnt working compleetly on hheroku for whatever reason

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,6 +69,7 @@ router.get("/dashboard", authCheck, async (req, res) => {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
